d637119166fbc3501d644e91ff2784d1f72e2d81
Zakariyya Mughal <zaki.mughal@gmail.com>
2020-01-25 15:45:29 -0600

    Rename s,p5-Renard-Block-Google-Translate,p5-Renard-Block-Service-Google-Translate,

---
+++ lib/Renard/Block/Service/Google/Translate.pm
@@ -0,0 +1,11 @@
+package Renard::Block::Service::Google::Translate;
+
+use strict;
+use warnings;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Block-Service-Google-Translate/>
+
+=cut
